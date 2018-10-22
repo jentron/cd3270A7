5d06909d7e5618feac84ed07a85fba5b1789eecc
Ron Jensen <ron@jentronics.com>
2018-10-21 22:58:36 -0600

    Replace main activity

--- app/src/main/java/com/jentronics/cs3270a8/MainActivity.java
+++ app/src/main/java/com/jentronics/cs3270a8/MainActivity.java
@@ -1,31 +1,29 @@
 package com.jentronics.cs3270a8;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.jentronics.cs3270a8.db.AppDatabase;
-import com.jentronics.cs3270a8.db.Course;
-
-import java.util.List;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                List<Course> courseList = AppDatabase.getInstance(getApplicationContext())
-                .courseDAO()
-                .getAll();
-
-        for(Course c : courseList) {
-            Log.d("Test", "All Courses: " + c.toString());
-        }
-            }
-        }).start();
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
