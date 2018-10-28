5bc43596de2bddb7f90113acf6c3a373af29c78a
Ron Jensen <ron@jentronics.com>
2018-10-28 14:20:21 -0600

    Left off here last week

---
+++ app/src/main/java/com/jentronics/cs3270a8/NewCourseDialogFragment.java
@@ -0,0 +1,10 @@
+package com.jentronics.cs3270a8;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.DialogFragment;
+
+public class NewCourseDialogFragment extends DialogFragment {
+    private TextInputEditText edt_id, edt_name, edt_course_code, edt_start_at, edt_end_at;
+
+
+}
